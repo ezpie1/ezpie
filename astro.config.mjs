b987e971f2e91d47a2bd44321cf4c09caf99cb9a
Ezpie <ezpie.co@gmail.com>
2023-06-06 17:34:32 +0530

    added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel(),
 });
